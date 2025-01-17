227190556e396833c704fc7901c2d525ace8c55f
alejandrojuarez117 <aljuarez117@gmail.com>
2025-01-16 18:22:43 -0700

    add dynamic fields for 'create' controller method

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -12,10 +12,10 @@ class ProductsController < ApplicationController
 
   def create
     @product = Product.new(
-    name: "Audio-Technica M50x Headphones",
-    price: 149,
-    image_url: "https://media.sweetwater.com/m/giveaway/2025/01/popup/0101-January-Giveaway-SitePopUp-1140-740.png?optimize=medium&quality=75&ha=155b5b6073b2f792",
-    description: "Closed back studio monitoring headphones."
+    name: params[:input_name],
+    price: params[:input_price],
+    image_url: params[:input_image_url],
+    description: params[:input_description]
     )
     @product.save
     render :show
