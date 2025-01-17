3933a5f45c08728613bd548b79c61730baa2a02c
alejandrojuarez117 <aljuarez117@gmail.com>
2025-01-16 17:59:11 -0700

    add REST fixes and create REST

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,4 +3,21 @@ class ProductsController < ApplicationController
     @products = Product.all
     render :index
   end
+
+  def show
+    id_find = params[:id]
+    @product = Product.find_by(id: id_find)
+    render :show
+  end
+
+  def create
+    @product = Product.new(
+    name: "Audio-Technica M50x Headphones",
+    price: 149,
+    image_url: "https://media.sweetwater.com/m/giveaway/2025/01/popup/0101-January-Giveaway-SitePopUp-1140-740.png?optimize=medium&quality=75&ha=155b5b6073b2f792",
+    description: "Closed back studio monitoring headphones."
+    )
+    @product.save
+    render :show
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -11,5 +11,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
-  get "/computer_parts" => "products#index"
+  get "/products" => "products#index"
+  get "/products/:id" => "products#show"
+  post "/products" => "products#create"
 end
