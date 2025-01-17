a0a4fb2727cee6e6fce9e76ed0ff17dfca092d69
alejandrojuarez117 <aljuarez117@gmail.com>
2025-01-16 18:59:51 -0700

    add dynamic 'REST' patch elements

--- config/routes.rb
+++ config/routes.rb
@@ -14,4 +14,5 @@ Rails.application.routes.draw do
   get "/products" => "products#index"
   get "/products/:id" => "products#show"
   post "/products" => "products#create"
+  patch "/products/:id" => "products#update"
 end
