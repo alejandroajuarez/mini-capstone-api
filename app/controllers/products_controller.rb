5cb56e7eafeaab1b1543112eae0622d497c82f7e
alejandrojuarez117 <aljuarez117@gmail.com>
2025-01-21 18:40:24 -0700

    add 'or' statement for product updating and rails validation

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -17,18 +17,25 @@ class ProductsController < ApplicationController
     image_url: params[:input_image_url],
     description: params[:input_description]
     )
-    @product.save
-    render :show
+    if @product.save
+      render :show
+    else
+      render json: { errors: @product.errors.full_messages }, status: :unprocessable_entity
+    end
   end
 
   def update
     @product = Product.find_by(id: params[:id])
-    @product.name = params[:name]
-    @product.price = params[:price]
-    @product.image_url = params[:image_url]
-    @product.description = params[:description]
-    @product.save
-    render :show
+    @product.name = params[:name]               || @product.name
+    @product.price = params[:price]             || @product.price
+    @product.image_url = params[:image_url]     || @product.image_url
+    @product.description = params[:description] || @product.description
+    
+    if @product.save
+      render :show
+    else
+      render json: { errors: @product.errors.full_messages }, status: :unprocessable_entity
+    end
   end
 
   def destroy
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,8 @@
 class Product < ApplicationRecord
+  validates :name, presence: true
+  validates :name, uniqueness: true
+  validates :price, presence: true
+  validates :price, numericality: { greater_than: 0 }
+  validates :description, presence: true
+  validates :description, length: { in: 50..500 }
 end
