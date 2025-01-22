1c17d01867860d4fa4f12d0d944e4aa1d0755d15
alejandrojuarez117 <aljuarez117@gmail.com>
2025-01-21 20:05:36 -0700

    add price methods

--- app/models/product.rb
+++ app/models/product.rb
@@ -4,5 +4,18 @@ class Product < ApplicationRecord
   validates :price, presence: true
   validates :price, numericality: { greater_than: 0 }
   validates :description, presence: true
-  validates :description, length: { in: 20..500 }
+  validates :description, length: { in: 5..500 }
+end
+
+def is_discounted?
+  if price < 10
+  end
+end
+
+def tax
+  (price * 0.09).round(2)
+end
+
+def total
+  (tax + price).round(2)
 end
