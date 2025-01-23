55193e1a8930b87bd97de4d160e4df6f513c5fa5
alejandrojuarez117 <aljuarez117@gmail.com>
2025-01-22 17:48:46 -0700

    add quantity column and updated all entries with quantities

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_01_16_013444) do
+ActiveRecord::Schema[7.2].define(version: 2025_01_23_003618) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -21,5 +21,6 @@ ActiveRecord::Schema[7.2].define(version: 2025_01_16_013444) do
     t.string "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "quantity"
   end
 end
