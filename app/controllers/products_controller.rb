fd56001f1c9c6a96e20ceaa5644fe9b336c7e4fc
alejandrojuarez117 <aljuarez117@gmail.com>
2025-02-12 20:16:03 -0700

    add supplier_id to product controller

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -17,7 +17,8 @@ class ProductsController < ApplicationController
     name: params[:name],
     price: params[:price],
     image_url: params[:image_url],
-    description: params[:description]
+    description: params[:description],
+    supplier_id: params[:supplier_id]
     )
     if @product.save
       render :show
@@ -32,8 +33,9 @@ class ProductsController < ApplicationController
     @product.price = params[:price]             || @product.price
     @product.image_url = params[:image_url]     || @product.image_url
     @product.description = params[:description] || @product.description
+    @product.supplier_id = params[:supplier_id] || @product.supplier_id
     
-    if @product.save
+    if @product.save!
       render :show
     else
       render json: { errors: @product.errors.full_messages }, status: :unprocessable_entity
