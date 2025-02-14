67c5a0c974dc443d82f4db2bef17a05e081c028c
alejandrojuarez117 <aljuarez117@gmail.com>
2025-02-13 17:51:33 -0700

    add order jbuilder and fix order controller

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,22 +1,26 @@
 class OrdersController < ApplicationController
 
   def index
-    
+    @orders = Order.all
+    render :index
   end
 
   def show
-    
+    id_find = params[:id]
+    @order = Order.find_by(id: id_find)
+    render :show
   end
 
   def create
-    
-  end
-
-  def update
-    
-  end
-
-  def delete
-    
+    @order = Order.new(
+      user_id: params[:user_id],
+      product_id: params[:product_id],
+      quantity: params[:quantity],
+      subtotal: params[:subtotal],
+      tax: params[:tax],
+      total: params[:total]
+    )
+    @order.save
+    render :show
   end
 end
